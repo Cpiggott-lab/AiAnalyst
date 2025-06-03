42240af49bea568fedf277b2daeb0e010cea4e82
Cpiggott-lab <crpapc@gmail.com>
2025-06-03 11:36:12 +0200

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,40 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import UploadPage from "./pages/UploadPage";
-import ProjectViewPage from "./pages/ProjectViewPage";
-import LandingPage from "./pages/LandingPage";
-import DashboardPage from "./pages/DashboardPage";
+import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import "./index.css";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow">
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/upload" element={<UploadPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/projects/:id" element={<ProjectViewPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
